7b954b05a7ec8d2c7c52839811669cd464e9f434
lisamurphy788 <lisamurphy788@gmail.com>
2021-11-08 22:09:44 +0000

    Add code for division

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,27 +29,26 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(gameType) {
-
+ function runGame(gameType){
     document.getElementById("answer-box").value = "";
     document.getElementById("answer-box").focus();
-
-
-    // Creates two random numbers between 1 and 25
+    // Create 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
+    // Multiply num1 and num2 together for the division game. The equation will be divNum1 / num2 = num1.
+    let divNum1 = num1 * num2;
+    if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract" ) {
+    } else if(gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if(gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    } else if(gameType === "division"){
+        displayDivisionQuestion(divNum1, num2);
+    } else{
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
+        throw `Unknown game type: ${gameType}, Aborting!`;
     }
-
 }
 
 /**
@@ -78,23 +77,22 @@ function checkAnswer() {
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
-function calculateCorrectAnswer() {
-
+ function calculateCorrectAnswer(){
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];
-    } else {
+    if(operator === "+"){
+        return[operand1 + operand2, "addition"];
+    } else if(operator === "-"){
+        return[operand1 - operand2, "subtract"];
+    } else if(operator === "x"){
+        return[operand1 * operand2, "multiply"];
+    } else if(operator === "/"){
+        return[operand1 / operand2, "division"];
+    }else{
         alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
+        throw `Unimplemented operator ${operator}, Aborting!`;
     }
-
 }
 
 /**
@@ -141,3 +139,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
+// Add your division question here
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+}
