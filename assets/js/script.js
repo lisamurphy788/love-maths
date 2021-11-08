86055fb75a62d0a8b61660ebfa1946d99f220bc9
lisamurphy788 <lisamurphy788@gmail.com>
2021-11-08 13:35:49 +0000

    Set up stucture of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+function runGame() {
+
+
+}
+
+function checkAnswer(){
+
+
+}
+
+function calculateCorrectAnswer(){
+
+
+}
+
+function incrementScore(){
+
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function displayAdditonQuestion(){
+
+
+}
+function displaySubtractQuestion(){
+
+
+}
+function displayMultiplyQuestion(){
+
+    
+}
